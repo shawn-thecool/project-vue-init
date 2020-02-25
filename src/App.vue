fa5bc2a92ccdcc8a7dd2cc802325ad6f1c102ea1
shawn-thecool <shawn-dev@naver.com>
2020-02-25 14:19:24 +0900

    add comp comm input-files

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
     app
+    <!-- <inputFile id="fileUploadTester" accept=".png, .jpeg" label-text="test" :input-hndr="inputHndr"></inputFile> -->
+    <input-file id="inputFileTest" :input-hndr="inputHndr"></input-file>
   </div>
 </template>
 
 <script>
-
+import InputFile from '@commons/inputFile/InputFile.vue'
 export default {
   name: 'App',
-  
+  methods: {
+    inputHndr() {
+      console.log('dd')
+    }
+  },
+  components: {
+    InputFile
+  }
 }
 </script>
 
---
+++ src/components/commons/inputFile/InputFile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="inp_file">
+    <label :for="id">{{ labelText }}</label>
+    <input type="file" :id="id" :accept="accept" @input="inputHndr" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comm-input-file',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    labelText: {
+      type: String,
+      default: null
+    },
+    accept: {
+      tpye: String,
+      default: null
+    },
+    inputHndr: {
+      type: Function,
+      default: () => null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.inp_file {
+}
+</style>

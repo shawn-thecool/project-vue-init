d051acfa5deb900dc33aa3a3f50464b2a467a4fc
shawn-thecool <shawn-dev@naver.com>
2020-02-27 16:42:19 +0900

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,22 @@ const router = new Router({
   routes: [
     { path: '/', name: 'home', component: Login },
     { path: '/login', name: 'login', component: Login },
-    { path: '/apply', name: 'apply', component: Apply },
-    { path: '/history', name: 'history', component: History },
-    { path: '*', redirect: '/' }
+    { path: '/apply', name: 'apply', component: Apply, meta: { requiresAuth: true } },
+    { path: '/history', name: 'history', component: History, meta: { requiresAuth: true } }
   ],
   scrollBehavior: () => ({ y: 0 })
+})
+
+router.beforeEach((to, from, next) => {
+  const hasRequiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  // TODO: login api process 처리 후 최종 반환
+  const isAuth = false
+
+  hasRequiresAuth
+    ? isAuth
+      ? next()
+      : next({ path: '/login', query: { redirect: to.fullPath } })
+    : next()
 })
 
 export default router

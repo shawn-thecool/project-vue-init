f1f1b3d88bb29931cca75057c1d453264a2f3f95
lukas.sin <lukas.sin@kakaocorp.com>
2020-02-28 12:54:12 +0900

    [FEAT] accordion, accordion panel 구현

---
+++ src/components/commons/dropdown/AccordionBunddle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="comm_accordion">
+      <!-- <accordion-panel 
+        v-for="(item, index) in itemList" 
+        :key="index"
+        :clickHndr="clickTarget(index)" 
+        :defautlToggle="index === currentOpenTarget"
+        class="item_accordion" 
+        >
+        <template v-slot:header>
+          {{item.value}}
+        </template>
+        <template v-slot:body>
+            {{item.value}}
+        </template>
+      </accordion-panel> -->
+    <slot :itemList="itemList" :clickTarget="clickTarget" :currentOpenTarget="currentOpenTarget"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comm-accordion',
+  data(){
+    return {
+      currentOpenTarget : null
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'title'
+    },
+    itemList: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods : {
+    clickTarget (targetIndex) {
+      return () => {
+        this.currentOpenTarget = targetIndex
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/commons/dropdown/AccordionPanel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="comm_accordion_panel">
+    <div class="item_accordion">
+      <button type="button" @click="toggle">
+        <slot name="header" :item="item"></slot>
+      </button>
+      <div class="body_item" v-show="isToggle">
+        <slot name="body" :item="item"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comm-accordion-panel',
+  props: {
+    title: {
+      type: String,
+      default: 'title'
+    },
+    item: {
+      type: Object,
+      default: () => ({
+        value: ''
+      })
+    },
+    clickHndr: {
+      type: Function,
+      default: () => null
+    },
+
+    defautlToggle: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    isToggle: {
+      get() {
+        return this.defautlToggle
+      },
+      set() {
+        this.clickHndr()
+      }
+    }
+  },
+  methods: {
+    toggle() {
+      this.isToggle = !this.isToggle
+    },
+  }
+}
+</script>
+
+<style></style>
